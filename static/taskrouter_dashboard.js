7303d5a67753cad8f8e84419815551f787bf07d2
HassanKallab <48729420+HassanKallab@users.noreply.github.com>
2019-09-05 17:08:51 -0400

    Update taskrouter_dashboard.js

--- static/taskrouter_dashboard.js
+++ static/taskrouter_dashboard.js
@@ -2,7 +2,7 @@ var taskrouterDashboard = new Vue({
   el: '#taskrouterDashboard',
   data: {
     headerMessage: 'TaskRouter Real-Time Dashboard',
-    loggedUser: "hassan@cloudnetworks.tech",
+    loggedUser: "ameer@twilio.com",
     userAuthenticated: false,
     syncStatus: "Disconnected",
     totalTasks: 0,
